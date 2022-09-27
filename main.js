cee03e3004417b36a0eef6750fcdb3e0d1b1c084
ananyakubal28 <91237049+ananyakubal28@users.noreply.github.com>
2022-09-26 20:28:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+updateCanvas(){
+    background-color;white;
+    Math.random[]
+    console.log()}
+
+    draw(check_sketch){
+        if(drawn_sketch=sketch){
+
+        }
+
+    }
+
+    check_sketch(){
+        if(condition){
+            counter++
+            }     
+            if(stroke=='black' || stroke=='red'){
+                line(30,40,25,50)
+                }     
+
+            
+
+    }
+
